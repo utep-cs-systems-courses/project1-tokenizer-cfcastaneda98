c066cbc1638ed7c05180b52114e2a657c0cc30b9
cfcastaneda98 <cfcastaneda@miners.utep.edu>
2020-09-24 21:09:54 -0600

    modified some errors and added some comments

--- src/history.c
+++ src/history.c
@@ -4,8 +4,8 @@
 
 List* init_history()
 {
-  List *list = (List*)malloc(sizeof(List));
-  list->root = (Item*)malloc(sizeof(Item));
+  List *list = (List*)malloc(sizeof(List)); /*Assigns a memory slot for the list*/
+  list->root = (Item*)malloc(sizeof(Item)); /*Assigns a memory slot for each item in the list*/
 
   return list;
 }
@@ -14,12 +14,12 @@ void add_history(List *list, char*str)
 {
   Item *current = list->root;
   int id = 1;
-  while(current->next != 0)
+  while(current->next != 0) /* Will check each item from the list*/
     {
       current = current->next;
       id++;
     }
-  current->next = malloc(sizeof(Item));
+  current->next = malloc(sizeof(Item)); /*Assigns a memory slot while adding the string*/
   current->next->str = str;
   current->next->id = id;
 }
@@ -28,7 +28,7 @@ char *get_history(List *list, int id)
 {
   Item *current = list->root;
 
-  while(current != 0)
+  while(current != 0) /*Will check through the entire list to look for current*/
     {
       if (current->id == id)
 	{
@@ -36,15 +36,15 @@ char *get_history(List *list, int id)
 	}
       current = current->next;
     }
-  return "ERROR: ID not found!";
+  return "ERROR: ID not found!"; /*This will print only if the item is not on the list*/
 }
 
 void print_history(List *list)
 {
-  Item *current = list->root->next;
-  while(current != 0)
+  Item *current = list->root->next; /*Bypasses the inital empty node*/
+  while(current != 0) /*Will check through the entire list to look for current*/
     {
-      printf("%d. %s\n",current->id,current->str);
+      printf("%d. %s\n",current->id,current->str); /*Prints items on the list along the way*/
       current = current->next;
     }
 }
@@ -52,14 +52,14 @@ void print_history(List *list)
 void free_history(List *list)
 {
   Item *current = list->root;
-  while(current->next != 0)
+  while(current->next != 0) /*Proceeds to free almost all the items until the last one*/
     {
-      Item *previous = current;
+      Item *previous = current;  
       current = current->next;
       free(previous->str);
       free(previous);
     }
-  free(current->str);
+  free(current->str); /*Will proceed to free the rest of the list, as well as the list itself*/
   free(current);
   free(list);
 }
