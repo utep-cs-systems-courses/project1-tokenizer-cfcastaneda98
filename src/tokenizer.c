94a92073b42533536546a951d0f5413a104f8aec
cfcastaneda98 <cfcastaneda@miners.utep.edu>
2020-09-25 20:42:06 -0600

    Added comments as well as alter some of the code

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -4,7 +4,7 @@
 
 int space_char(char c)
 {
-  if (c == " ")
+  if (c == " " || c == "\t") /*Will return a true value if the character is a space or a tab*/ 
     {
       return 1;
     }
@@ -13,7 +13,7 @@ int space_char(char c)
 
 int non_space_char(char c)
 {
-  if (c != " ")
+  if (c != " " || c != "\t") /*Will return a true value if the character is NOt a space or tab*/
     {
       return 1;
     }
@@ -22,7 +22,7 @@ int non_space_char(char c)
 
 char *word_start(char *str)
 {
-  while(space_char(*str))
+  while(space_char(*str)) /*Will process while it is a space character*/
     {
       str++;
     }
@@ -31,7 +31,7 @@ char *word_start(char *str)
 
 char *word_terminator(char *word)
 {
-  while(non_space_char(*word))
+  while(non_space_char(*word)) /*Will process while it is NOT a space character*/
     {
       word++;
     }
@@ -44,11 +44,11 @@ int count_words(char *str)
   char *words = word_start(str);
   while(*words != "\0")
     {
-      if(non_space_char(*p))
+      if(non_space_char(*p)) /*Will add one to the counter if it goes to a word*/
 	{
-	  counter++;
+	  counter++; 
 	}
-      words = word_terminator(words);
+      words = word_terminator(words); /*Will proceed to got to the end and start of the word*/
       words = word_start(words);
     }
   return counter;
@@ -56,12 +56,12 @@ int count_words(char *str)
 
 char *copy_str(char *inStr, short len)
 {
-  char *copyStr = malloc((len+1) * sizeof(char));
+  char *copyStr = malloc((len+1) * sizeof(char)); /*Creates a memory slot for the string*/
   for(int i = 0; i<len; i++)
     {
       copyStr[i] = inStr[i];
     }
-  copyStr[len] = "\0";
+  copyStr[len] = "\0"; /*Makes the last element of the string into a null value*/
   return copy;
 }
 
@@ -71,36 +71,34 @@ char **tokenizer(char* str)
   char **tokens = malloc((numWords+1) * sizeof(char*));
   char *word = str;
 
-  for(int i = 0;i<numWords;i++)
+  for(int i = 0; i<numWords; i++) /*Cycles through each word and creates a memory slot for each one*/
     {
-      word = word_start(word);
+      word = word_start(word); 
       int len = word_length(word);
       tokens[i] = copy_str(word,len);
       word = word_terminator(word);
     }
-  tokens[i] = 0;
+  tokens[i] = NULL; 
 
   return tokens;
 }
 
 void print_tokens(char **tokens)
 {
-  char **current = tokens;
-  while(*current != 0)
+  for (int i = 0; tokens[i] !=0; i++)
     {
-      printf("%s", *curr);
-      curr++;
+      printf("%s\n", tokens[i]); /*Prints each word one by one, and leaves a space afterwards*/
     }
   printf("\n");
 }
 
 void free_tokens(char **tokens)
 {
-  char *current = *tokens;
-  while(curr != 0)
+  char **current = tokens;
+  while(*current != 0)
     {
-      free(current);
-      current++;
+    free(*current);
+    current++;
     }
   free(tokens);
 }
@@ -108,7 +106,7 @@ void free_tokens(char **tokens)
 int string_length(char *str)
 {
   int len = 0;
-  for(int i = 0; *(str+i) != "\0"; i++)
+  for(int i = 0; *(str+i) != "\0"; i++) /*Will continue counting until the current space is null*/
     {
       len += 1;
     }
@@ -118,7 +116,7 @@ int string_length(char *str)
 int word_length(char *str)
 { 
   int len = 0;
-  for(int i = 0; (*(str+i) != "\0"; i++)
+  for(int i = 0; (*(str+i) != "\0"; i++) /*Will continue counting until current space is null*/
     {
       if(non_space_char(*(str+i)))
         {
@@ -126,7 +124,7 @@ int word_length(char *str)
 	}
       else
 	{
-	  break;
+	  break; /*Finished cycle as a space has been located*/
 	}
     }
     return len;
